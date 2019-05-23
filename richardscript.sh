5f6c16884cab81239cb6136d65d254b4bbdd6a47
markziemann <mark.ziemann@gmail.com>
2019-05-23 17:12:45 +1000

    initial commit

--- richardscript.sh
+++ richardscript.sh
@@ -2,7 +2,7 @@
 
 for (( c=1; c<=100; c++ )) do 
 
-formatdb -p F -o T -i 1_sample.fa
+formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
 
 msbar -sequence 1_sample.fa -count 300 -point 4 -block 0 -codon 0 -outseq 1_sample_mutated.fa
 
